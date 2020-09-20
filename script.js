9e46a6aad97d83e08bdb5298f6aaf38cfc700266
Zachary Lynn <zalynn25@hotmail.com>
2020-09-20 15:30:48 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var counter = 60;
+var question = [
+   {q1: "JavaScript is used to make websites interactive.",
+    r1: "True",
+    r2: "False"},
+
+   {q2: "CSS stands for 'cool style sheet'.",
+    r1: "True",
+    r2: "False"},
+
+    {q3: "HTML is a type of markup language.",
+    r1: "True",
+    r2: "False"},
+
+    {q4: "",
+    r1: "True",
+    r2: "False"},
+
+    {q5: "",
+    r1: "True",
+    r2: "False"},
+
+    {q6: "",
+    r1: "True",
+    r2: "False"},
+
+]
 
 
-alert("hello")
+document.getElementById("startBtn").addEventListener("click", function startTimer(){
+    alert("All questions are True/False.")
+    
+
+});
